dab0883d074f73155dba9c599ea238c325d8c7e6
vinukumar26 <124863943+vinukumar26@users.noreply.github.com>
2023-07-14 22:59:37 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,6 @@ from flask import Flask
 abi=Flask(__name__)
 @abi.route('/')
 def a():
-    return "Hello WORLD"
+    return "HEY PUSSY-CAT"
 if __name__ == "__main__":
     abi.run(debug=True,host='0.0.0.0')
